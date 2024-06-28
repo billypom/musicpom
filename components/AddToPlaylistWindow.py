aa7c749d84795d2e04527db49b8b2c5ce7fa4ff0
billypom on debian <billypompey@icloud.com>
2024-06-27 22:33:41 -0400

    add to playlist widget working

--- components/AddToPlaylistWindow.py
+++ components/AddToPlaylistWindow.py
@@ -1,4 +1,5 @@
 from PyQt5.QtWidgets import (
+    QDialog,
     QWidget,
     QListWidget,
     QFrame,
@@ -10,24 +11,26 @@ from PyQt5.QtWidgets import (
 from PyQt5.QtGui import QFont
 
 
-class AddToPlaylistWindow(QListWidget):
+class AddToPlaylistWindow(QDialog):
     def __init__(self, list_options: dict):
         super(AddToPlaylistWindow, self).__init__()
-        # self.setWindowTitle("Choose")
-        # self.setMinimumSize(400, 400)
+        self.setWindowTitle("Choose")
+        self.setMinimumSize(400, 400)
+        layout = QVBoxLayout()
+        listWidget = QListWidget(self)
         for k, v in list_options:
-            self.addItem(f"{k} | {v}")
-        # layout = QVBoxLayout()
+            listWidget.addItem(f"{k} | {v}")
         #
-        # label = QLabel("Playlists")
-        # label.setFont(QFont("Sans", weight=QFont.Bold))
-        # layout.addWidget(label)
-        # layout.addWidget(self)
+        label = QLabel("Playlists")
+        label.setFont(QFont("Sans", weight=QFont.Bold))
+        layout.addWidget(label)
+        layout.addWidget(listWidget)
 
         # Save button
-        # save_button = QPushButton("Add")
-        # save_button.clicked.connect(self.save)
-        # layout.addWidget(save_button)
+        save_button = QPushButton("Add")
+        save_button.clicked.connect(self.save)
+        layout.addWidget(save_button)
+        self.setLayout(layout)
         self.show()
 
     def save(self):
